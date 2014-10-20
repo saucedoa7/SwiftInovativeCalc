f9f940dde1eb65420dab4c5e0fe6a5c0456adf16
Albert Saucedo <saucedoa7@gmail.com>
2014-10-20 18:44:58 -0500

    Initial Commit

---
+++ SwiftInovativeCalc/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SwiftInovativeCalc
+//
+//  Created by Albert Saucedo on 10/20/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ SwiftInovativeCalc/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  SwiftInovativeCalc
+//
+//  Created by Albert Saucedo on 10/20/14.
+//  Copyright (c) 2014 Albert Saucedo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
